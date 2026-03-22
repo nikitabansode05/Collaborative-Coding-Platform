16be4d2b642aba8d8929ee8e3f5ecb98baa23baa
Nikita Bansode <bansodenikita66@gmail.com>
2026-03-22 20:57:25 +0530

    Collaborative coding platform header

---
+++ Frontend/CollaborativeCodeFrontend/src/App.jsx
@@ -0,0 +1,9 @@
+import Header from "./components/layout/Header";
+
+export default function App() {
+  return (
+    <div>
+      <Header />
+    </div>
+  );
+}
---
+++ Frontend/CollaborativeCodeFrontend/src/components/layout/Header.jsx
@@ -0,0 +1,28 @@
+import {Link} from 'react-router-dom';
+
+function Header() {
+  return (
+    <header>
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div className="container-fluid">
+          {/* Brand Logo */}
+          <h2 className="navbar-brand mb-0">Collaborative Coding Platform</h2>
+
+          {/* Navigation Links */}
+          <div className="collapse navbar-collapse">
+            <ul className="navbar-nav ms-auto">
+              <li className="nav-item">
+                <Link to="/create" className="nav-link">Create Team</Link>
+              </li>
+              <li className="nav-item">
+                <Link to="/show" className="nav-link">Show Teams</Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
